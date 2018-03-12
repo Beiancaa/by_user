f6218077eb5b7b9231c8ab4cb75cb517e84bce37
Beiancaa <beiancayousif89@gmail.com>
2018-03-12 12:58:58 +0300

    add user fields and bootstrap4

--- db/migrate/20180312073417_create_users.rb
+++ db/migrate/20180312073417_create_users.rb
@@ -2,6 +2,21 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :father_name
+      t.string :grandpa_name
+      t.string :family_name
+      t.date :birthday
+      t.string :gender
+      t.integer :locality
+      t.integer :alley
+      t.integer :house_number
+      t.string :neighborhood 
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :cell_phone_number
+      t.string :home_phone_number
+      t.string :email_address
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,21 @@ ActiveRecord::Schema.define(version: 20180312073417) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "father_name"
+    t.string "grandpa_name"
+    t.string "family_name"
+    t.date "birthday"
+    t.string "gender"
+    t.integer "locality"
+    t.integer "alley"
+    t.integer "house_number"
+    t.string "neighborhood"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "cell_phone_number"
+    t.string "home_phone_number"
+    t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

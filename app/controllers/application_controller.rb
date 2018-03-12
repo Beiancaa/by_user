07bc0bc38b489d20a151f0c4938662cb946cbb8b
Beiancaa <beiancayousif89@gmail.com>
2018-03-12 19:28:43 +0300

    create user in arabic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+  
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
+  
+  private
+  
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+    #|| I18n.default_locale
+    #Rails.application.routes.default_url_options[:locale]= I18n.locale
+  end
 end
